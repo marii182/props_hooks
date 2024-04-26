1abe22e15901641f690ecd147231a93412c4951a
marii182 <maria.sivaxavier18@gmail.com>
2024-04-26 15:24:10 -0300

    props e hooks

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Card from "./components/Card/Card"
+import Contador from "./components/Contador/Contador"
+import Task from "./components/Task/Task"
+
+function App() {
+  return (
+   <>
+     {/* <Card titulo="React" descricao="Biblioteca com JS"/>
+     <Card titulo="Spring" descricao="Framework do Java"/> */}
+
+     {/* <Contador /> */}
+
+     <Task />
+   </>
+  )
+}
+
+export default App
---
+++ src/components/Contador/Contador.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import './Contador.css'
+
+function Contador() {
+
+  const [valor, setValor] = useState(0); //Variável de estado
+ // let valor: number = 0;
+
+function somarMaisUm() {
+  setValor(valor + 1)
+    //valor = valor+1;
+}
+
+  return (
+    <div className="container">
+        <p>O valor é: {valor}</p>
+        <button onClick={somarMaisUm}>Adicionar +1</button>
+    </div>
+  )
+}
+
+export default Contador
